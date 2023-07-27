be7128d7d542c0217f0655d79af1a63a566007e3
Austin Rhode <austinrhode7@gmail.com>
2023-07-26 22:09:56 -0400

    Updated file pathing and added favicon image

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
+const favicon = require('serve-favicon');
 
 const PORT = process.env.PORT || 3000;
 
-const path = require('path')
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname,'public', 'favicon.ico')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
